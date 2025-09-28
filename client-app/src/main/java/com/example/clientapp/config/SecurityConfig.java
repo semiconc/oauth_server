4ed0e52c84812ea4ccdaa63b9239ae1c2c97c8ea
semiconc <semiconc@gmail.com>
2025-09-28 17:01:02 +0900

    Create SecurityConfig.java

---
+++ client-app/src/main/java/com/example/clientapp/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.clientapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
+}
